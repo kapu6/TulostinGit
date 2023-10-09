1e63b5ab1baf244e870a8cc830389eae390c9179
Julius Eronen <julius23001@student.hamk.fi>
2023-10-09 16:41:14 +0300

    Laskutoimituksen printtaus

--- src/TulostinGitti.java
+++ src/TulostinGitti.java
@@ -13,7 +13,11 @@ public class TulostinGitti {
 		System.out.println("luku1-muuttujan arvo on " + luku1); 
 		System.out.println("luku2-muuttujan arvo on " + luku2);
 		
-		
+		System.out.print(luku1);
+		System.out.print(" + ");
+		System.out.print(luku2);
+		System.out.print(" = ");
+		System.out.println(summa);
 
 	}
 
